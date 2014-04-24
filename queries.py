554c81accaaf5fd9ac2097cd19b3ac454eca96c0
Steven Maude <steve@scraperwiki.com>
2014-04-24 10:53:42 +0100

    Initial commit of query class

---
+++ queries.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class SparqlQuery(object):
+    def __init__(self, offset=0, limit=1000):
+        self.query_template = None
+        self.json_result = None
+        self.offset = offset
+        self.limit = limit
+        self.query = self._make_query()
+
+    def _make_query(self):
+        """ Builds a query using template,"""
+        raise NotImplementedError
+
+    def submit_query(self, endpoint_url='https://knowledgestore.fbk.eu'
+                                        '/nwrdemo/sparql'):
+        """ Submit query to endpoint; return result. """
