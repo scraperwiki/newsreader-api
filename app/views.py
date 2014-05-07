8ad6e9e0bdd4f906e4bf63fa757cb3b0ddc744cb
Steven Maude <steve@scraperwiki.com>
2014-05-07 13:24:29 +0100

    Run query 3 via Flask.
    Visit /test/query3 and it returns the JSON of the result.

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 from flask import render_template, request
 from app import app
+import query3
 
 
 @app.route('/')
@@ -17,3 +18,9 @@ def handle_offset_and_limit():
     limit = request.args.get('limit')
     return render_template("query3.html", title='Query 3', offset=offset,
                            limit=limit)
+
+
+@app.route('/test/query3')
+def run_test_query():
+    query = query3.create_sparql_query(query3.sparql_template_array, 1)
+    return str(query3.perform_sparql_query(query))
