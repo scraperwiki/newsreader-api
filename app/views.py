62653900a644b3e5a5b41e129844fa5975414eb8
Steven Maude <steve@scraperwiki.com>
2014-05-12 13:30:19 +0100

    Add pagination code for sample query
    Taken from http://flask.pocoo.org/snippets/44/ (including changes in
    comments).

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from flask import render_template, request
+from flask import abort, render_template, request, url_for
 from app import app
 from collections import namedtuple
+from pagination import Pagination
 import query3
+
+PER_PAGE = 10
 
 
 @app.route('/')
@@ -46,6 +49,39 @@ def run_test_query():
     result_count = get_query_3_result_count()
     return render_template("parsed_query3.html", title='Query 3 results',
                             count=result_count, results=parsed_results)
+
+
+@app.route('/testpaging', defaults={'page': 1})
+@app.route('/testpaging/page/<int:page>')
+def show_query3_results(page):
+    # TODO: avoid calling count more than once, expensive (though OK if cached)
+    count = get_query_3_result_count()
+    results = get_results_for_page(page, PER_PAGE)
+
+    if not results and page != 1:
+        abort(404)
+
+    pagination = Pagination(page, PER_PAGE, int(count))
+    return render_template('paginate.html', title='Query 3 results',
+                           pagination=pagination, count=count,
+                           results=results)
+
+
+def get_results_for_page(page, results_per_page):
+    offset = results_per_page * (page - 1)
+    # TODO: use limit of 100 by default for now; consider allowing variation
+    query = query3.create_sparql_query(query3.sparql_template_array, 1,
+                                       offset, limit=results_per_page)
+    json_query_results = query3.perform_sparql_query(query)
+    return parse_query3_results(json_query_results)
+
+
+def url_for_other_page(page):
+    args = dict(request.view_args.items() + request.args.to_dict().items())
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
 
 
 def get_query_3_result_count():
