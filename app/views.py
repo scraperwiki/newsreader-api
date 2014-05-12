fdd8ced4e75797c827426e255363b196288051d2
Steven Maude <steve@scraperwiki.com>
2014-05-12 10:52:06 +0100

    Move getting query string parameters into function
    Repeated code for getting offset and limit.

--- app/views.py
+++ app/views.py
@@ -12,17 +12,23 @@ def index():
     return "Hello World!"
 
 
+def get_offset_and_limit_from_query():
+    """ Return offset and limit from query. """
+    return request.args.get('offset'), request.args.get('limit')
+
+
 @app.route('/simple/query3')
 def handle_offset_and_limit():
     """ Simple query string function. """
-    offset = request.args.get('offset')
-    limit = request.args.get('limit')
+    offset, limit = get_offset_and_limit_from_query()
     return render_template("query3.html", title='Query 3 test query string',
                            offset=offset, limit=limit)
 
 
 @app.route('/test/query3')
 def run_test_query():
+    offset, limit = get_offset_and_limit_from_query()
+
     query = query3.create_sparql_query(query3.sparql_template_array, 1)
     json_query_results = query3.perform_sparql_query(query)
     parsed_results = parse_query3_results(json_query_results)
