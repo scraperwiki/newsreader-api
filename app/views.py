7002025e815cc8b374c17e9137cbcf6b83ddd054
Steven Maude <steve@scraperwiki.com>
2014-05-12 15:09:39 +0100

    Move non-pagination code out to a separate file.
    For tidiness, for now.

--- app/views.py
+++ app/views.py
@@ -15,42 +15,6 @@ def index():
     return "Hello World!"
 
 
-def get_offset_and_limit_from_query():
-    """ Return offset and limit from query. """
-    try:
-        offset = int(request.args.get('offset'))
-    except TypeError:
-        offset = 0
-
-    try:
-        limit = int(request.args.get('limit'))
-    except TypeError:
-        limit = 100
-
-    return offset, limit
-
-
-@app.route('/simple/query3')
-def handle_offset_and_limit():
-    """ Simple query string function. """
-    offset, limit = get_offset_and_limit_from_query()
-    return render_template("query3.html", title='Query 3 test query string',
-                           offset=offset, limit=limit)
-
-
-@app.route('/test/query3')
-def run_test_query():
-    offset, limit = get_offset_and_limit_from_query()
-
-    query = query3.create_sparql_query(query3.sparql_template_array, 1)
-    json_query_results = query3.perform_sparql_query(query)
-    parsed_results = parse_query3_results(json_query_results)
-
-    result_count = get_query_3_result_count()
-    return render_template("parsed_query3.html", title='Query 3 results',
-                            count=result_count, results=parsed_results)
-
-
 @app.route('/testpaging', defaults={'page': 1})
 @app.route('/testpaging/page/<int:page>')
 def show_query3_results(page):
