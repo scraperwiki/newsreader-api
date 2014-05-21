96d02929070169fd7936fb23f1f07c11fd76ef9e
Steven Maude <steve@scraperwiki.com>
2014-05-21 11:38:11 +0100

    Refactor to enable output format choice

--- app/views.py
+++ app/views.py
@@ -18,35 +18,45 @@ def index():
 
 @app.route('/<query_to_use>', defaults={'page': 1})
 @app.route('/<query_to_use>/page/<int:page>')
-def show_query_results(page, query_to_use):
-    # TODO: avoid calling count more than once, expensive (though OK if cached)
+def run_query(page, query_to_use):
+    """ Return response of selected query using query string values. """
     query_name = getattr(queries, query_to_use)
     query_args = parse_query_string(**request.args)
-    query = query_name(**query_args)
-    count = query.get_total_result_count()
+
     offset = PER_PAGE * (page - 1)
-    results = get_results_for_page(query_name, PER_PAGE, offset, **query_args)
+    current_query = query_name(offset=offset, limit=PER_PAGE, **query_args)
+    current_query.submit_query()
 
-    if not results and page != 1:
+    cause_404_if_no_results(current_query.parse_query_results(), page)
+    return produce_response(current_query, page, offset)
+
+
+def produce_response(query, page_number, offset):
+    """ Get desired result output from completed query; create a response. """
+    # TODO: avoid calling count more than once, expensive (though OK if cached)
+    if query.output == 'json':
+        return json.dumps(query.json_result)
+    else:
+        count = query.get_total_result_count()
+        pagination = Pagination(page_number, PER_PAGE, int(count))
+        result = query.parse_query_results()
+        return render_template(query.jinja_template,
+                               title=query.query_title,
+                               pagination=pagination,
+                               count=count,
+                               results=result, offset=offset+1)
+
+
+def cause_404_if_no_results(results, page_number):
+    """ If results is an empty string or None, cause 404. """
+    # TODO: Look into this; doesn't seem to apply for the SPARQL responses.
+    if not results and page_number != 1:
         abort(404)
-
-    pagination = Pagination(page, PER_PAGE, int(count))
-    # TODO: May need to handle template differently for different queries.
-    # Could consider making title, template.html argument part of the class
-    return render_template(query.jinja_template, title=query.query_title,
-                           pagination=pagination, count=count,
-                           results=results, offset=offset+1)
 
 
 def parse_query_string(**kwargs):
     """ Take request.args and return a dict for passing in as **kwargs """
     return {key: json.loads(request.args.get(key)) for key in kwargs}
-
-
-def get_results_for_page(query_name, results_per_page, offset, **query_args):
-    query = query_name(offset=offset, limit=results_per_page, **query_args)
-    query.submit_query()
-    return query.parse_query_results()
 
 
 def url_for_other_page(page):
