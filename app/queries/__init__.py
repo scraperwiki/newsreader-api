cc9f390ba205c246a3e40383b1294969326a386d
Ian Hopkinson <ian@scraperwiki.com>
2014-05-28 17:37:38 +0100

    Typo fixing

--- app/queries/__init__.py
+++ app/queries/__init__.py
@@ -6,11 +6,11 @@ __all__ = ["properties_of_a_type"]
 import queries
 
 from .properties_of_a_type import properties_of_a_type
-from .types_of_actor import types_of_actor
+from .types_of_actors import types_of_actors
 from .event_details_filtered_by_actor import event_details_filtered_by_actor
 from .describe_uri import describe_uri
 from .actors_of_a_type import actors_of_a_type
 from .property_of_actors_of_a_type import property_of_actors_of_a_type
 from .summary_of_events_with_actor import summary_of_events_with_actor
-from .actors_sharing_an_event_with_an_actor import (
-                        .actors_sharing_an_event_with_an_actor)
+from .actors_sharing_event_with_an_actor import (
+                                        actors_sharing_event_with_an_actor)
