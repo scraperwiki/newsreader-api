a3e94a0be6c42e962c5feee2b762a9efd6f56313
Ian Hopkinson <ian@scraperwiki.com>
2014-05-30 08:01:11 +0100

    Progress on metadata query, currently not getting action in URL

--- app/queries/get_document_metadata.py
+++ app/queries/get_document_metadata.py
@@ -9,18 +9,20 @@ class get_document_metadata(CRUDQuery):
 
     http://127.0.0.1:5000/get_document_metadata?uris.0=<uri>
     """
+    # https://knowledgestore.fbk.eu/nwr/worldcup-hackathon/resources?id=http://news.bbc.co.uk/sport2/hi/football/gossip_and_transfers/5137822.stm 
     def __init__(self, *args, **kwargs):
         super(get_document_metadata, self).__init__(*args, **kwargs)
         self.query_title = 'Get document metadata'
         self.query_template = ("""{uri_0}""")
         self.count_template = ("""""")
-
+        self.action = "resources"
+        
         self.jinja_template = 'table.html'
         
         self.headers = ['property']
 
         self.required_parameters = ["uris"]
-        self.optional_parameters = ["output", "offset", "limit"]
+        self.optional_parameters = []
         self.number_of_uris_required = 1
 
         self.query = self._build_query()
