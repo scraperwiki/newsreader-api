f69e3cb731bf17a0720da20ade710b13ad59009d
Ian Hopkinson <ian@scraperwiki.com>
2015-01-13 19:20:46 +0000

    Add an eso query, really just add eso prefix but made a separate query for consistency

---
+++ app/queries/summary_of_events_with_eso.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import unicode_literals
+
+from queries import SparqlQuery
+
+class summary_of_events_with_eso(SparqlQuery):
+
+    """ Get events containing a specified eso value
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(summary_of_events_with_eso, self).__init__(*args, **kwargs)
+        self.query_title = 'Get events with a specific eso value'
+        self.description = 'eso is an ontology which encompases various semantic frameing schemes which give an indication as to the character of an event'
+        self.url = 'summary_of_events_with_eso'
+        self.example = 'summary_of_events_with_eso?uris.0=eso:Renting'
+        self.query_template = ("""
+SELECT ?event (COUNT(*) AS ?event_size) ?datetime ?event_label
+WHERE {{
+  {{
+    SELECT DISTINCT ?event ?datetime ?event_label
+    WHERE {{
+      ?event a sem:Event .
+      ?event rdfs:label ?event_label .
+      ?event rdf:type {uri_0} .
+      ?event sem:hasTime ?t . 
+      ?t owltime:inDateTime ?d .
+      {date_filter_block}
+      ?t rdfs:label ?datetimetmp .
+      FILTER (regex(?datetimetmp,"\\\\d{{4}}-\\\\d{{2}}"))
+      BIND (SUBSTR(?datetimetmp,1,10) AS ?datetime)
+    }}
+    ORDER BY ?datetime
+    OFFSET {offset}
+    LIMIT {limit}
+  }}
+  ?event ?p ?o .
+}}
+GROUP BY ?event ?datetime ?event_label
+ORDER BY ?datetime
+                               """)
+
+        self.count_template = ("""
+SELECT (COUNT(DISTINCT ?event) AS ?count)
+WHERE {{
+  {{
+    SELECT DISTINCT ?event ?datetime
+    WHERE {{
+      ?event a sem:Event .
+      ?event rdfs:label ?event_label .
+      ?event rdf:type {uri_0} .
+      ?event sem:hasTime ?t . 
+      ?t owltime:inDateTime ?d .
+      {date_filter_block}
+      ?t rdfs:label ?datetimetmp .
+      FILTER (regex(?datetimetmp,"\\\\d{{4}}-\\\\d{{2}}"))
+      BIND (SUBSTR(?datetimetmp,1,10) AS ?datetime)
+    }}
+    ORDER BY ?datetime
+  }}
+  ?event ?p ?o .
+}}
+                               """)
+
+        self.jinja_template = 'table.html'
+        self.headers = ['event', 'datetime', 'event_label', 'event_size']
+
+        self.required_parameters = ["uris"]
+        self.optional_parameters = ["output", "offset", "limit", "datefilter"]
+        self.number_of_uris_required = 1
+
+        self.query = self._build_query()
