eaf81da367fd3921328b6c2f1b31d80ab686db02
Ian Hopkinson <ian@scraperwiki.com>
2015-01-16 20:31:46 +0000

    Add a test generator to run all the example queries

---
+++ app/test_integration.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+from app import app
+from app import make_documentation
+
+def test_generator():
+    root_url = ""
+    endpoint_path = '/cars'
+    function_list = make_documentation.CarsDocsCreator(root_url, endpoint_path).make_docs()
+    for query in function_list['queries']:
+        yield check_query, query['example']
+
+def check_query(query):
+    router = app.test_client()
+    rv = router.get(query)
+    assert 'error' not in rv.data.decode('UTF-8')
+
+# This is how we tested counts
+# def test_13_summary_of_events_with_event_label(self):
+#        rv = self.app.get('/summary_of_events_with_event_label?filter=bribe&datefilter=2010&output=json')
+#        json_form = json.loads(rv.data)
+#        assert_equal(json_form['count'], 317)
